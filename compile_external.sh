3fdf262c9d6a873c65b25b130e35f513792bf801
Michael de Lang <kingoipo@gmail.com>
2017-05-25 23:19:21 +0200

    Add compile dependencies script

---
+++ compile_external.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CPUS="$( grep -c ^processor /proc/cpuinfo )"
+cd $DIR/external/common_backend
+./compile_external.sh
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+make -j$CPUS
+cd $DIR/external/uWebSockets
+make
+cd $DIR
